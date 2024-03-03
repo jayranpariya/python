9882adb7af4b1b0dd40edcc56b18b9b94ead73f4
jayranpariya <jayranapriya04@gmail.com>
2024-03-03 14:11:32 +0530

    module 1,2,3 is Done

---
+++ 03 Control Flow/14 Exercice.py
@@ -0,0 +1,19 @@
+# 11 Exercice
+
+# Display the even number ( 2 4 6 8) followed by this message "We have 4 even numbers"
+# 2
+# 4
+# 6
+# 8
+# We have 4 even numbers
+
+# Mosh anwser
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count = count + 1
+        print(number)
+print(f"we have {count} even number")
+
+
+# In my anwser I did not count the numbers
